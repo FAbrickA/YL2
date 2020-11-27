d0060438e20e5de634fb8280a32ab644f12bbaa4
Shilonosov Alexandr <schilonosov.alex@yandex.ru>
2020-11-27 07:53:04 +0300

    Work commit

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtCore import Qt, QPoint
-from PyQt5.uic import loadUi
 import sys
 from random import randint
+from UI import Ui_MainWindow
 
 
-class MyApp(QMainWindow):
+class MyApp(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
         self.circles = []
-        self.yellow = QColor(255, 246, 6)
 
-        loadUi("UI.ui", self)
+        self.setupUi(self)
 
         self.button.clicked.connect(self.button_handler)
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        pen = QPen(self.yellow, 10, Qt.SolidLine)
-        qp.setPen(pen)
         for circle in self.circles:
-            x, y, d = circle
+            x, y, d, color = circle
             center = QPoint(x, y)
+            pen = QPen(color, 10, Qt.SolidLine)
+            qp.setPen(pen)
             qp.drawEllipse(center, d, d)
         qp.end()
 
     def button_handler(self):
         x, y = randint(20, self.width() - 20), randint(20, self.height() - 20)
         d = randint(10, 120)
-        self.circles.append((x, y, d))
+        color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.circles.append((x, y, d, color))
         self.repaint()
 
 
